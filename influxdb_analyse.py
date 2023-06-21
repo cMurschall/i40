4ce4f8972e6e778978964663bcc2f7b7fda4b32d
Christian Murschall <christian@murschall.net>
2023-06-21 23:27:22 +0200

    Add calculation of round trip time

--- influxdb_analyse.py
+++ influxdb_analyse.py
@@ -17,7 +17,6 @@ def read_environment_variables():
     return env_vars
 
 
-
 def read_shellies():
     environment_variables = read_environment_variables()
 
@@ -43,6 +42,30 @@ def read_shellies():
     return df
 
 
+def read_signal(signal):
+    environment_variables = read_environment_variables()
+
+    organization = environment_variables["DOCKER_INFLUXDB_INIT_ORG"]
+    token = environment_variables["DOCKER_INFLUXDB_INIT_ADMIN_TOKEN"]
+
+    client = InfluxDBClient(url="http://localhost:8086", token=token, org=organization)
+
+    query = client.query_api()
+
+    q = f"""from(bucket: "AdlerTasksNewData")
+      |> range(start: 0, stop: now())
+      |> filter(fn: (r) => r["_measurement"] == "DS_SS23_TrayStates_B")
+      |> filter(fn: (r) => r["Sensor"] == "{signal}")
+      |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
+      |> yield() """
+
+    df = query.query_data_frame(q)
+
+    df["_time"] = pd.to_datetime(df['_time'])
+    df = df.drop(columns=["_start", "_stop"]) # they are useless
+    return df
+
+
 def find_step():
     df = read_shellies()
     data = df["value"].to_numpy()
@@ -51,13 +74,13 @@ def find_step():
     step = np.hstack((np.ones(len(data)), -1 * np.ones(len(data))))
 
     data_step = np.convolve(data, step, mode='valid')
-    step_index= np.argmax(data_step)
+    step_index = np.argmax(data_step)
 
     step_time = df["_time"].loc[step_index]
 
     # plots
     fig, ax1 = plt.subplots()
-    ax1.plot(df["_time"], data_step[:-1] / 10,color='salmon')
+    ax1.plot(df["_time"], data_step[:-1] / 10, color='salmon')
 
     ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
     ax2.plot(df["_time"], df["value"], color='yellowgreen')
@@ -68,14 +91,37 @@ def find_step():
     ax1.text(step_time, data_step[step_index] / 10, step_time.strftime("%m.%d. %H:%M Uhr"), color='r')
     plt.show()
 
+
+def analyze_waggons():
+    # signal_10B3 = read_signal("10B3")
+    signal_cam = read_signal("KameraP")
+
+    print(f"Total time {(signal_cam['_time'].max() - signal_cam['_time'].min()).total_seconds():.2f} s")
+
+    # we cast to int
+    signal_cam["value"].astype("int32")
+    # ignore waggon 0
+    signals_cam = signal_cam[signal_cam["value"] != 0]
+
+    df_times = pd.DataFrame(columns=['delta_time', 'waggon'])
+    for index, row in signals_cam[:-1].iterrows():
+        # for each waggon let's find the next occurrence
+        for index_n, row_n in signals_cam[index :].iterrows():
+            if row_n["value"] == row["value"]:
+                new_item = {
+                    'delta_time' : (row_n["_time"] - row["_time"]).total_seconds(),
+                    'waggon' : row["value"]
+                }
+                df_times = pd.concat([df_times, pd.DataFrame([new_item])])
+                break
+    print(f"Mean roundtrip time: {df_times['delta_time'].mean():.2f} s")
+    print(f"Standard deviation roundtrip time: {df_times['delta_time'].std():.2f} s")
+
+
+
+
+
+
 if __name__ == '__main__':
-    find_step()
-
-
-    #fig, ax = plt.subplots()
-    #plt.plot(df["_time"], df["value"])
-
-    #ax.xaxis.set_major_formatter(DateFormatter('%d.%m.'))
-    #_ = plt.xticks(rotation=90)
-
-    #plt.show()
+    # find_step()
+    analyze_waggons()
